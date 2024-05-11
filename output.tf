d26b1fcbeaca2d7364675917fd41b11283473049
naresh <Naresh@gmail.com>
2024-05-11 19:17:35 +0530

    route53 module added to git repo

---
+++ output.tf
@@ -0,0 +1,6 @@
+# output "private_ip" {
+#     value = data.aws_instance.private_ip
+# }
+# output "public_ip" {
+#     value = data.aws_instance.public_ip
+# }
---
+++ vairables.tf
@@ -0,0 +1,16 @@
+variable "zone_id" {
+    type = string
+}
+variable "name" {
+    type = string
+}
+variable "ttl" {
+    type = number
+    default = 10
+}
+variable "records" {
+
+}
+variable "type" {
+  
+}
